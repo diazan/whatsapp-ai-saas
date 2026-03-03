ee9292ed604aa4229cdee9f6b2a332615c809acf
diazan <34430483+diazan@users.noreply.github.com>
2026-03-03 16:48:14 -0500

    edit get and add post

--- src/server.js
+++ src/server.js
@@ -13,6 +13,22 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("WhatsApp AI SaaS running ✅");
+app.get("/webhook", (req, res) => {
+  const verifyToken = "estetica_verify_2026";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === verifyToken) {
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook recibido:");
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
 });
