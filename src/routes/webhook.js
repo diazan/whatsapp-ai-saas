22b5a317abcf73ece35de5f7a6a9fa24392b015e
diazan <34430483+diazan@users.noreply.github.com>
2026-03-05 08:29:44 -0500

    updated files

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { handleWebhook } = require("../controllers/webhookController");
+const { handleWebhook } = require("../controllers/webhookControllers");
 
 router.post("/", handleWebhook);
 
